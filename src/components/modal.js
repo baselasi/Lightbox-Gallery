d1d3eefae59178ef787b6c4a2ee614fec9b9ee07
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-05-31 08:49:02 +0200

    Update modal.js

--- src/components/modal.js
+++ src/components/modal.js
@@ -3,10 +3,7 @@ import React from "react";
 
 
 export default function Modal(url){
-
-
-
     return(
-        <div style={{backgroundImage:`url(${url})`}} className="modal"></div>
+        <div style={{backgroundImage:`url(${url.url})`}} className="modal"></div>
     )
 }
