caac911eb25322a5391c31d7bf79fd28fdee5615
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-05-31 10:27:33 +0200

    Update gid.js

--- src/components/gid.js
+++ src/components/gid.js
@@ -2,20 +2,31 @@ import React from "react";
 import data from "./data";
 import "./style.css"
 import Modal from "./modal";
+import Gallery from "./gallery";
 export default function Grid(){
     const [images,setImges] = React.useState([data])
-    const [state, setState] = React.useState(false)
+    const [state, setState] = React.useState(true)
     const [activeImage, setActiveImage] = React.useState("")
     function handelClcik(url){
         setActiveImage(url)
+        setState((prevState)=>!prevState)
+        console.log(state)
     }
-    console.log(data)
-    
     return(
         <div className="main">
-            {data.map((el)=><div style={{backgroundImage:`url(${el})`}} onClick={()=>handelClcik(el)}>fasf</div>)}
+            {data.map((el)=>
+                <div   
+                    style={{backgroundImage:`url(${el})`}}
+                    onClick={()=>handelClcik(el)}  
+                    className="image">
+                </div>
+            )}
             <Modal
             url={activeImage}
+            active={state}
+            />
+            <Gallery
+            Gallery={data}
             />
         </div>
     )
