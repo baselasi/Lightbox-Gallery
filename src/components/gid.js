6689f6f10437e9d3116ccc2865a640699e0dde96
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-05-31 16:12:55 +0200

    Update gid.js

--- src/components/gid.js
+++ src/components/gid.js
@@ -5,34 +5,44 @@ import Modal from "./modal";
 import Gallery from "./gallery";
 export default function Grid(){
     const [images,setImges] = React.useState([data])
-    const [state, setState] = React.useState(true)
+    const [state, setState] = React.useState(false)
     const [activeImage, setActiveImage] = React.useState("")
     function handelClcik(url){
         setActiveImage(url)
         setState((prevState)=>!prevState)
-        console.log(state)
     }
     function changeImage(ul){
         setActiveImage(ul)
     }
+    function changState(){
+       if(state){
+        setState(false)
+       }
+    }
     return(
-        <div className="main">
-            {data.map((el)=>
-                <div   
-                    style={{backgroundImage:`url(${el})`}}
-                    onClick={()=>handelClcik(el)}  
-                    className="image">
+        <>
+            <div className="body" onClick={changState}>
+                <div className="main" >
+                    {data.map((el)=>
+                        <div   
+                            style={{backgroundImage:`url(${el})`}}
+                            onClick={()=>handelClcik(el)}  
+                            className="image">
+                        </div>
+                    )}
                 </div>
-            )}
-            <Modal
-            url={activeImage}
-            active={state}
-            />
-            <Gallery
-            gallery={data}
-            changeImage={changeImage}
-            state={state}
-            />
-        </div>
+            </div>
+            <div>
+                <Modal
+                url={activeImage}
+                active={state}
+                />
+                <Gallery
+                gallery={data}
+                changeImage={changeImage}
+                state={state}
+                />
+            </div>
+        </>
     )
 }
