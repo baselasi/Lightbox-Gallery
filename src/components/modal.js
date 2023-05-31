6ef796a2ee89c1e20591709cb76fff8d3d3b1aec
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-05-31 10:27:29 +0200

    Update modal.js

--- src/components/modal.js
+++ src/components/modal.js
@@ -2,8 +2,20 @@ import React from "react";
 
 
 
-export default function Modal(url){
+export default function Modal(url,active,gallery){
+    //{url.gallery.map((img)=><div style={{backgroundImage:`${img}`}}></div>)}
+    console.log(url.gallery)
     return(
-        <div style={{backgroundImage:`url(${url.url})`}} className="modal"></div>
+        
+            <div style={{
+                backgroundImage:`url(${url.url})`,
+                display: url.active ? "none" : "flex"
+                }} className="modal">
+            </div>
+            
+        
+            
+        
+        
     )
 }
