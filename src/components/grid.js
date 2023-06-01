60b5292a413a97f281b56f5f2641b14bb7f93358
bassel-new <128236840+bassel-new@users.noreply.github.com>
2023-06-01 22:45:57 +0200

    Update grid.js

--- src/components/grid.js
+++ src/components/grid.js
@@ -4,20 +4,19 @@ import "./style.css"
 import Modal from "./modal";
 import Gallery from "./gallery";
 export default function Grid(){
-    const [images,setImges] = React.useState([data])
-    const [state, setState] = React.useState(false)
-    const [activeImage, setActiveImage] = React.useState("")
-    function handelClcik(url){
+    const [state, setState] = React.useState(false)             //state to identify whether a image in being shown in the modal or not
+    const [activeImage, setActiveImage] = React.useState("")     //state to identify the image in the modal  
+    function handelClcik(url){                  //choose the image to be shown by clicking it from the grid     
         setActiveImage(url)
         setState((prevState)=>!prevState)
     }
-    function changeImage(ul){
+    function changeImage(ul){           //choose the image to be shown by cliking it from the gallery below
         setActiveImage(ul)
     }
-    function changState(){
+    function changState(){      //hide the image after cliking outside the modal
        if(state){
         setState(false)
-       }
+        }
     }
     return(
         <>
@@ -25,7 +24,7 @@ export default function Grid(){
                 <div className="main" >
                     {data.map((el)=>
                         <div   
-                            style={{backgroundImage:`url(${el})`}}
+                            style={{backgroundImage:`url(${el})`}}  
                             onClick={()=>handelClcik(el)}  
                             className="image">
                         </div>
